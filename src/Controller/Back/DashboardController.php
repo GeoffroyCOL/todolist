e70c277a5c1273c3154ac65a93d3cc6af3297665
GeoffroyCOL <geoffroy.colpart81@gmail.com>
2021-06-02 15:48:14 +0200

    admin-page: Création de la page dashboard. Ajout d el'extension twig extra-string.

--- src/Controller/Back/DashboardController.php
+++ src/Controller/Back/DashboardController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controller\Back;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\ProjectService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * DashboardController
@@ -14,16 +15,24 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
  */
 class DashboardController extends AbstractController
 {    
+    public function __construct(
+        private ProjectService $projectService
+    )
+    {}
+
     /**
      * @return Response
      */
     #[Route('/admin/dashboard', name: 'dashboard')]
     public function index(): Response
     {
+        $user = $this->getUser();
+
         return $this->render('back/dashboard/index.html.twig', [
             'controller_name'   => 'DashboardController',
             'current_page'      => 'dashboard',
-            'component'         => 'admin'
+            'component'         => 'admin',
+            'projects'          => $projects = $this->projectService->getProjectsByUser($user)
         ]);
     }
 }
